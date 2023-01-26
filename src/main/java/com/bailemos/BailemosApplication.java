b7d5100640882158b8cfe8d44a79e6b54049dc49
FlavianIuga <121820069+FlavianIuga@users.noreply.github.com>
2023-01-26 18:15:21 +0200

    Initial commit!

---
+++ src/main/java/com/bailemos/BailemosApplication.java
@@ -0,0 +1,13 @@
+package com.bailemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BailemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BailemosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bailemos/BailemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bailemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BailemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
