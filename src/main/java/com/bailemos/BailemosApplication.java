b8f5c8ea1143d3366deb3e04bbc518d499289c35
FlavianIuga <121820069+FlavianIuga@users.noreply.github.com>
2023-03-20 16:30:40 +0200

    Fixing application.properties

--- src/main/java/com/bailemos/BailemosApplication.java
+++ src/main/java/com/bailemos/BailemosApplication.java
@@ -2,8 +2,9 @@ package com.bailemos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BailemosApplication {
 
 	public static void main(String[] args) {
