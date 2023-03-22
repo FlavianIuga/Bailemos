bc7757c7f8f5cb3e3aa1b12cd3a159e67cb19747
FlavianIuga <121820069+FlavianIuga@users.noreply.github.com>
2023-03-22 19:52:08 +0200

    Adding registration microservice

---
+++ registration-microservice/src/main/java/com/bailemos/RegistrationMicroservice.java
@@ -0,0 +1,12 @@
+package com.bailemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationMicroservice {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RegistrationMicroservice.class, args);
+  }
+}
